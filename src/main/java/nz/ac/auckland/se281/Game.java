07bcbdccd2d5536b5bf7cd6c0a1dc6f6973848ef
Kieran Joe <kjoe685@aucklanduni.ac.nz>
2024-05-15 11:00:33 +1200

    Starting a new round now increments the round counter and prints a message

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,13 +6,20 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  private int roundNumber; // the round of the game
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
 
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+
+    roundNumber = 0;
   }
 
-  public void play() {}
+  public void play() {
+    roundNumber += 1;
+    MessageCli.START_ROUND.printMessage(Integer.toString(roundNumber));
+  }
 
   public void endGame() {}
 
