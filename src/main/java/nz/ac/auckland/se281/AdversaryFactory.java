4face4d4b6c95c78239b89e6447ee8532bc2171b
Kieran Joe <kjoe685@aucklanduni.ac.nz>
2024-05-16 12:04:49 +1200

    added JavaDoc comments to methods in AdversaryFactory

--- src/main/java/nz/ac/auckland/se281/AdversaryFactory.java
+++ src/main/java/nz/ac/auckland/se281/AdversaryFactory.java
@@ -3,7 +3,17 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class AdversaryFactory {
+
+  /**
+   * Creates and returns an adversary of the appropriate Adversary subclass based on the given
+   * difficulty
+   *
+   * @param difficulty the difficulty of the game
+   * @return adversary object corresponding to the given difficulty
+   */
   public static Adversary makeAdversary(Difficulty difficulty) {
+
+    // return an adversary of the subclass corresponding to the difficulty given
     switch (difficulty) {
       case EASY:
         return new EasyAdversary();
